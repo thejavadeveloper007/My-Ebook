998b76e886447e0781700fd05b4c8eb49f8ccc9f
thejavadeveloper007 <thejavadeveloper007@gmail.com>
2023-12-18 10:32:35 +0530

    merging from my main account

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { Header } from "./Layouts/Header";
+export { Footer } from "./Layouts/Footer";
+
+export { ProductCard } from "./Elements/ProductCard";
+export { Rating } from "./Elements/Rating";
+
+export { DropdownLoggedIn } from "./Elements/DropdownLoggedIn";
+export { DropdownLoggedOut } from "./Elements/DropdownLoggedOut";
+export { Loader } from "./Elements/Loader";
+
+export { ScrollToTop } from "./Other/ScrollToTop";
